18d1b619a2bd91ce5a849a5c9c0849e4406a37a6
james.wang <james.wang@cellopoint.com>
2023-05-24 16:53:11 +0800

    新增測試靜態網站 完整檔案

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      class="px-3"
+      color="white"
+      flat
+      density="compact"
+    >
+      <v-spacer></v-spacer>
+
+      <v-tabs
+        centered
+        color="grey-darken-2"
+      >
+        <v-tab
+          v-for="link in links"
+          :key="link"
+        >
+          {{ link }}
+        </v-tab>
+      </v-tabs>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Dashboard',
+        'Messages',
+        'Profile',
+        'Updates',
+      ],
+    }),
+  }
+</script>
